0204fbf118da6be5358ccbb719dd6b7d39df5c0e
aemrehu <aemrehu@gmail.com>
2024-03-17 20:06:12 +0200

    add logging to server and client

--- src/server.py
+++ src/server.py
@@ -1,7 +1,12 @@
 import socket
+import logging
 import threading
 import json
 from pathlib import Path
+
+# Configure logging
+logging.basicConfig(filename='server.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
 class Server:
     def __init__(self):
@@ -14,42 +19,39 @@ class Server:
         self.peers = {}  # Dict to keep track of connected peers
         self.index = 0
 
-    def handle_peer(self, peer):
-        while True:
-            data = peer.recv(1024).decode('ascii')
-            if not data:
-                break
-
-            data = json.loads(data)
-
-            if 'list' in data:
-                # Handle list data
-                peer.send(json.dumps(self.peers).encode('ascii'))
-                continue
+        logging.info('Server initialized')
 
     def start_server(self):
         def accept_peers():
             while True:
                 data, addr = self.sock.recvfrom(1024)
+                logging.info(f"Received {data.decode()} from {addr}")
                 if data.decode() == 'punch':
                     if addr not in self.peers.values():
                         self.peers[int(self.index)] = addr
                         self.index += 1
-                    print(f"Punched hole for {addr}")
+                        self.sock.sendto('punched'.encode('ascii'), addr)
+                        logging.info(f"Punched hole for {addr}")
+                        print(f"Punched hole for {addr}")
                 if data.decode() == 'list':
                     # self.sock.sendto(json.dumps(self.peers).encode('ascii'), addr)
                     for i, peer in self.peers.items():
                         if peer != addr:
                             self.sock.sendto(f'{i}: {peer}'.encode('ascii'), addr)
+                    logging.info(f"Sent list of peers to {addr}")
                 if data.decode().split(' ')[0] == 'peer':
                     i = int(data.decode().split(' ')[1])
                     self.sock.sendto(json.dumps(self.peers[i]).encode('ascii'), addr)
+                    logging.info(f"Sent peer {i} to {addr}")
 
         threading.Thread(target=accept_peers).start()
+        logging.info('Server started')
 
 if __name__ == "__main__":
     server = Server()
     threading.Thread(target=server.start_server).start()
+
+    logging.info('Server is running')
 
     # wait for q to quit
     while True:
