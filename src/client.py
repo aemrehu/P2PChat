3a93b6bd7d4c7eb2273d7f58252fa7879eb26081
Tomi Pantsar <tomi___00@hotmail.com>
2024-03-17 12:43:37 +0200

    add port selection logic

--- src/client.py
+++ src/client.py
@@ -4,39 +4,50 @@ import threading
 
 class Client:
 
-    sport = 50001
-
     get_peer = False
-
     approved_peers = []
-
-    ip = ""
-    dport = 0
-
+    dest_ip = ""
+    sender_port = 0  # Sender port (50001 by default)
+    dest_port = 50000  # Destination port
     sock = None
 
-    def __init__(self) -> None:
-        pass
-
     def run(self):
-        print("Please enter the server's IP address and port in the format 'ip:port'")
-        address = input()
-        self.ip, port = address.split(":")
-        self.dport = int(port)
-        self.approved_peers.append((self.ip, self.dport))
+        try:
+            self.dest_ip = socket.gethostbyname(socket.gethostname())
+        except socket.gaierror:
+            print("Hostname could not be resolved. Exiting")
+            sys.exit()
+
+        print(f"DEBUG: ip = {self.dest_ip}")
+        print(f"DEBUG: port = {self.dest_port}")
+
+        self.approved_peers.append((self.dest_ip, self.dest_port))
 
         print("Punching hole")
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(('0.0.0.0', self.sport))
-        self.sock.sendto(b'punch', (self.ip, self.dport))
-
+        
+        self.sender_port = self.find_available_sender_port(50001, 60000)
+        self.sock.bind(('0.0.0.0', self.sender_port)) # Bind to any available port
+        print(f"DEBUG: binded to {self.dest_ip}:{self.sender_port}")
+        
+        self.sock.sendto(b'punch', (self.dest_ip, self.dest_port))
         print("Hole punched")
 
         listener = threading.Thread(target=self.listen, daemon=True)
         listener.start()
 
         self.send_msg()
+
+    def find_available_sender_port(self, start_port, end_port):
+        for port in range(start_port, end_port + 1):
+            try:
+                with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                    s.bind(('0.0.0.0', port))
+                return port
+            except OSError:
+                pass
+        raise OSError("Could not find an available port in the range.")
 
     def listen(self):
         while True:
@@ -49,21 +60,21 @@ class Client:
             if addr in self.approved_peers:
                 print(f"{addr}: {data.decode()}")
 
-    def connect_to_peer(self, addr):
-        self.sock.sendto(b'punch', addr)
-        print(f"Punched hole for {addr}")
+    def connect_to_peer(self, address):
+        self.sock.sendto(b'punch', address)
+        print(f"Punched hole for {address}")
 
-        self.approved_peers.append(addr)
+        self.approved_peers.append(address)
 
-        self.ip = addr[0]
-        self.dport = addr[1]
+        self.dest_ip = address[0]
+        self.dest_port = address[1]
 
-        print(f"DEBUG: {self.ip}, {self.dport}")
+        print(f"DEBUG: {self.dest_ip}, {self.dest_port}")
 
     def send_msg(self):
         while True:
             msg = input('> ')
-            self.sock.sendto(msg.encode(), (self.ip, self.dport))
+            self.sock.sendto(msg.encode(), (self.dest_ip, self.dest_port))
 
             if msg == 'peer':
                 self.get_peer = True
